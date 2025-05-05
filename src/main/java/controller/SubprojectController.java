c8d47b3bd3773a810c6664accac1a2caec3ea1af
Daniel23894 <daso0005@stud.kea.dk>
2025-05-05 10:13:28 +0200

    Add SubprojectService import to SubprojectController for service integration

--- src/main/java/controller/SubprojectController.java
+++ src/main/java/controller/SubprojectController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import service.ProjectService;
+import service.SubprojectService;
 
 import java.util.List;
 
