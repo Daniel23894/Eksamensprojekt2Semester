342b8cb0f80bcba164345c3a5e48f87fa74ac6e3
Daniel23894 <daso0005@stud.kea.dk>
2025-05-07 02:12:04 +0200

    Update project and subproject status display to use description field in HTML and enhance StateStatus enum with descriptions

--- src/main/java/model/StateStatus.java
+++ src/main/java/model/StateStatus.java
@@ -1,20 +1,26 @@
 package model;
 
 public enum StateStatus {
-    NOT_STARTED(0),
-    IN_PROGRESS(1),
-    COMPLETED(2),
-    PAUSED(3),
-    CANCELLED(4);
+    NOT_STARTED(0, "Ikke startet"),
+    IN_PROGRESS(1, "I gang"),
+    COMPLETED(2, "Afsluttet"),
+    PAUSED(3, "Pauset"),
+    CANCELLED(4, "Annulleret");
 
     private final int value;
+    private final String description;
 
-    StateStatus(int value){
+    StateStatus(int value, String description){
         this.value = value;
+        this.description = description;
     }
 
     public int getValue(){
         return value;
+    }
+
+    public String getDescription(){
+        return description;
     }
 
     public static StateStatus fromValue(int value){
